fbaf31b94b0986a3d1cc274e099921fcd990b603
P2FU2 <66651175+P2FU2@users.noreply.github.com>
2023-08-08 05:47:55 -0300

    site quase pronto todas as paginas foaram criadas e a estrtura feita

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const projectLinks = document.querySelectorAll('.project-list a');
+
+  projectLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+          // Remove a classe 'active' de qualquer outro link
+          projectLinks.forEach(innerLink => innerLink.classList.remove('active'));
+
+          // Adiciona a classe 'active' ao link clicado
+          link.classList.add('active');
+
+          // Opcional: para evitar que o link realmente navegue (se for o caso)
+          event.preventDefault();
+      });
+  });
+});
